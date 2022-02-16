d46a7364921f3e51b82acc6333464305a6be10a1
Pat Bar <59734015+Joberooo@users.noreply.github.com>
2022-02-16 14:10:40 +0100

    Add calc Damage method

---
+++ src/com/company/CalcDamage.java
@@ -0,0 +1,60 @@
+package com.company;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class CalcDamage {
+    private final Map<String, DamageRelation> damageByType = new HashMap<>();
+
+    public CalcDamage() throws IOException {
+
+        String[] types = new String[]{"fire", "ground", "rock", "water", "bug", "steel", "grass", "ice", "fairy", "dragon", "psychic", "fighting", "poison", "dark", "normal"};
+        for(String type : types){
+            damageByType.put(type, new DamageRelation(type));
+        }
+    }
+
+    public void calcCompleteDamage(String attackerType, String[] defenderTypes){
+        float damage = 1;
+
+        DamageRelation attackerDamageRelation = damageByType.get(attackerType);
+
+        for(String defenderType : defenderTypes){
+            damage = calcSingleDamage(attackerDamageRelation, defenderType, damage);
+        }
+
+        System.out.println(damage + "x");
+    }
+
+    public void calcCompleteDamage(String attackerType, String defenderType){
+        DamageRelation attackerDamageRelation = damageByType.get(attackerType);
+
+        float damage = calcSingleDamage(attackerDamageRelation, defenderType, 1);
+
+        System.out.println(damage + "x");
+    }
+
+    private float calcSingleDamage(DamageRelation attackerDamageRelation, String defenderType, float damage){
+        for(String type : attackerDamageRelation.doubleDamageTo){
+            if(Objects.equals(type, defenderType)){
+                damage *= 2;
+            }
+        }
+
+        for(String type : attackerDamageRelation.halfDamageTo){
+            if(Objects.equals(type, defenderType)){
+                damage *= 0.5;
+            }
+        }
+
+        for(String type : attackerDamageRelation.noDamageTo){
+            if(Objects.equals(type, defenderType)){
+                damage *= 0;
+            }
+        }
+
+        return damage;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,16 @@
 package com.company;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String[] types = new String[]{"fire", "ground", "rock", "water", "bug", "steel", "grass", "ice", "fairy", "dragon"};
-        Map<String, DamageRelation> damageByType = new HashMap<>();
-        for(String type : types){
-            damageByType.put(type, new DamageRelation(type));
-        }
-        System.out.println(damageByType);
+        CalcDamage calcDamage = new CalcDamage();
+
+        calcDamage.calcCompleteDamage("fire", "grass");
+        calcDamage.calcCompleteDamage("fighting", new String[]{"ice", "rock"});
+        calcDamage.calcCompleteDamage("psychic", new String[]{"poison", "dark"});
+        calcDamage.calcCompleteDamage("water", "normal");
+        calcDamage.calcCompleteDamage("fire", "rock");
     }
 }
