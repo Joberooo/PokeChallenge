16e046eb64ed99f01d776c1f06412fc63c099df3
Pat Bar <59734015+Joberooo@users.noreply.github.com>
2022-02-16 13:51:18 +0100

    Add hashmap with all damages by type

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,17 @@
 package com.company;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         String[] types = new String[]{"fire", "ground", "rock", "water", "bug", "steel", "grass", "ice", "fairy", "dragon"};
-        DamageRelation fireType = new DamageRelation("fire");
-        System.out.println(fireType);
+        Map<String, DamageRelation> damageByType = new HashMap<>();
+        for(String type : types){
+            damageByType.put(type, new DamageRelation(type));
+        }
+        System.out.println(damageByType);
     }
 }
